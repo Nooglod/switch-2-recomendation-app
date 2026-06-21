2279847c7bd80f92e4bde9081350959d5431cffe
Dulguun Batsaikhan <98036660+Nooglod@users.noreply.github.com>
2026-06-21 16:56:43 +0900

    finalfinal

--- games.js
+++ games.js
@@ -11,7 +11,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/t-iNpDKuYb8",
-    "summary": "몰입감 있는 스토리 좋아하는 사람 긴 플레이 타임의 RPG 원하는 사람 캐릭터 성장과 전략 전투 좋아하는 사람",
+    "summary": "서로 적이었던 두 나라의 병사, 노아와 미오가 손을 잡다. 죽음이 강요되는 세계의 진실에 맞서는 100시간짜리 대서사 RPG",
     "features": [
       "실시간 파티 전투",
       "광활한 오픈필드",
@@ -27,11 +27,11 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/RSGgCfbYrg0",
-    "summary": "",
+    "summary": "유령이 들끓는 호텔에 마리오 일행이 납치됐다! 겁쟁이 루이지가 폴터거스트로 유령을 빨아들이며 17개 층을 탐험한다",
     "features": [
-      "퍼즐 탐험 요소",
       "유령 청소 액션",
-      "협동 플레이"
+      "퍼즐 탐험 요소",
+      "협동 플레이 지원"
     ],
     "image": "images/game2.svg",
     "en": "Luigi's Mansion 3"
@@ -43,10 +43,10 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/OV1gjqhHB2s",
-    "summary": "",
+    "summary": "마리오 캐릭터들의 화끈한 테니스 대결! 개성 넘치는 특수 기술과 다채로운 코트에서 펼쳐지는 스포츠 액션",
     "features": [
       "다양한 캐릭터 플레이",
-      "액션 스포츠 경기",
+      "특수 기술 테니스",
       "멀티플레이 지원"
     ],
     "image": "images/game3.svg",
@@ -59,7 +59,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/hXx-ZY60fN4",
-    "summary": "",
+    "summary": "총알이 지배하는 던전 '건전'을 뚫어라! 매번 달라지는 방 배치와 수백 종의 총기로 '과거를 죽이는 총'을 찾아라",
     "features": [
       "탄막 액션 전투",
       "로그라이크 슈팅",
@@ -75,10 +75,10 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFJ",
     "youtube": "https://www.youtube.com/embed/ohClxMmNLQQ",
-    "summary": "",
+    "summary": "이혼 위기의 부부가 인형으로 변해버렸다! 매 스테이지마다 전혀 다른 게임 장르로 협력하며 관계를 회복하는 감동 어드벤처",
     "features": [
-      "다양한 장르 구성",
       "2인 전용 협동 플레이",
+      "다양한 장르 구성",
       "감성적인 스토리"
     ],
     "image": "images/game5.svg",
@@ -91,10 +91,10 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFJ",
     "youtube": "https://www.youtube.com/embed/k9xHZXiLvLY",
-    "summary": "편안한 게임 좋아하는 사람 커플·가족 플레이 원하는 사람 귀여운 감성 좋아하는 사람",
+    "summary": "판지와 수공예품으로 만들어진 세상을 요시와 탐험하라! 뒷면까지 있는 스테이지에서 숨겨진 스탬프를 모으는 귀여운 힐링 어드벤처",
     "features": [
-      "쉬운 액션 플레이",
       "수공예 감성 맵",
+      "쉬운 액션 플레이",
       "협동 플레이 지원"
     ],
     "image": "images/game6.svg",
@@ -107,10 +107,10 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFJ",
     "youtube": "https://www.youtube.com/embed/fcwngWPXQtg",
-    "summary": "",
+    "summary": "서로 다른 꿈이 뒤섞인 두 작가의 이야기! SF와 판타지를 오가는 환상적인 세계를 2인 협동으로 탈출하는 어드벤처",
     "features": [
+      "2인 협동 플레이",
       "다양한 세계 변화",
-      "핵심 게임 설명",
       "퍼즐 액션 요소"
     ],
     "image": "images/game7.svg",
@@ -123,7 +123,7 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/j60ynVkvgMw",
-    "summary": "",
+    "summary": "종이 캐릭터를 서로 잘라 퍼즐을 풀어라! 창의적이고 엉뚱한 협동 퍼즐로 웃음이 터지는 파티 게임",
     "features": [
       "간단한 조작 방식",
       "협동 퍼즐 플레이",
@@ -139,10 +139,10 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/0R_e6GdaOwE",
-    "summary": "",
+    "summary": "은하계에 새로운 위협이 등장했다. 사무스 아란이 미지의 행성을 1인칭으로 탐험하며 능력을 강화하는 긴장감 넘치는 SF 액션",
     "features": [
-      "SF 세계관 분위기",
       "1인칭 탐험 액션",
+      "SF 세계관 분위기",
       "능력 확장 시스템"
     ],
     "image": "images/game9.svg",
@@ -155,7 +155,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/JStAYvbeSHc",
-    "summary": "마리오 입문자 친구와 협동 플레이 좋아하는 사람 밝고 경쾌한 액션 원하는 사람",
+    "summary": "원더 플라워 하나가 모든 걸 바꾼다! 매 스테이지마다 예측 불가의 원더 이펙트가 터지는 신선한 2D 마리오 어드벤처",
     "features": [
       "협동 플레이 지원",
       "창의적인 스테이지",
@@ -171,7 +171,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTJ",
     "youtube": "https://www.youtube.com/embed/UAO2urG23S4",
-    "summary": "",
+    "summary": "폐허가 된 벌레 왕국 '할로우네스트'의 비밀을 파헤쳐라! 정체 불명의 기사가 어둡고 아름다운 지하 세계를 탐험하는 소울라이크 어드벤처",
     "features": [
       "자유로운 탐험",
       "고난도 액션",
@@ -187,7 +187,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/U8lJRcUeEMs",
-    "summary": "",
+    "summary": "하데스의 딸 멜리노에가 지하 세계를 뚫고 시간의 신 크로노스에 맞서라! 매 도전마다 새로운 빌드로 즐기는 중독성 강한 로그라이크",
     "features": [
       "빠른 템포 전투",
       "로그라이크 액션",
@@ -203,7 +203,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/brmmV3g4qqo",
-    "summary": "",
+    "summary": "이상 공간에서 출현한 키메라로부터 도시를 지켜라! 특수 경찰이 정신 에너지로 연결된 전사 '리전'과 함께 펼치는 화려한 듀얼 액션",
     "features": [
       "SF 세계관",
       "듀얼 액션 전투",
@@ -219,7 +219,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/sZNeut6RR4I",
-    "summary": "친구들과 놀 게임 찾는 사람 가족 모임용 게임 원하는 사람 가볍고 웃긴 게임 좋아하는 사람",
+    "summary": "최대 8명이 함께 즐기는 초대형 파티 배틀! 보드게임과 100여 종의 미니게임으로 매판 새로운 왁자지껄 승부가 펼쳐진다",
     "features": [
       "최대 8인 플레이",
       "대규모 파티 게임",
@@ -235,11 +235,11 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/3pE23YTYEZM",
-    "summary": "\"사라진 전지메기를 찾고, 지하 세계의 비밀을 파헤쳐라!\" 혼돈의 대도시 '카오폴리스'의 신입 대원 3호가 되어, 의문의 액체로 가득 찬 미지의 세계 '얼터너'를 모험하세요!",
+    "summary": "전 세계를 무대로 달리는 역대 최대 마리오 카트! 광활한 오픈 월드 코스에서 최대 24명과 아이템 배틀 레이싱",
     "features": [
+      "오픈 월드 레이싱",
       "다양한 아이템 배틀",
-      "스피드 레이싱",
-      "멀티플레이 지원"
+      "최대 24인 멀티플레이"
     ],
     "image": "images/game16.svg",
     "en": "Mario Kart World"
@@ -251,10 +251,10 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/mQqVCYDD3sA",
-    "summary": "",
+    "summary": "음식으로 가득 찬 달콤한 코스를 누가 먼저 달릴까! 커비들이 케이크와 딸기를 모으며 겨루는 귀엽고 달콤한 파티 레이스",
     "features": [
-      "다양한 미니게임",
       "캐주얼 파티 레이스",
+      "다양한 미니게임",
       "귀여운 비주얼 감성"
     ],
     "image": "images/game17.svg",
@@ -267,11 +267,11 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/GUYDXVDLmns",
-    "summary": "\"사라진 전지메기를 찾고, 지하 세계의 비밀을 파헤쳐라!\" 혼돈의 대도시 '카오폴리스'의 신입 대원 3호가 되어, 의문의 액체로 가득 찬 미지의 세계 '얼터너'를 모험하세요!",
+    "summary": "사라진 전지메기를 찾고, 지하 세계의 비밀을 파헤쳐라! 대도시 '카오폴리스'의 신입 대원 3호가 되어 미지의 세계 '얼터너'를 모험하세요",
     "features": [
-      "변신 액션",
-      "영역 배틀",
-      "다양한 콘텐츠"
+      "잉크로 영역 점령",
+      "오징어 변신 액션",
+      "4인 협동 새몬 런"
     ],
     "image": "images/game18.svg",
     "en": "Splatoon 3"
@@ -283,7 +283,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/wVFnPHyAyPg",
-    "summary": "",
+    "summary": "5대5 팀 전투로 더 많은 점수를 먼저 채워라! 포켓몬을 키우며 팀과 협력해 상대 골대를 공략하는 전략 MOBA",
     "features": [
       "포켓몬별 역할 분담",
       "팀 기반 MOBA 전투",
@@ -295,14 +295,14 @@ window.GAMES = [
   {
     "id": "game20",
     "name": "오버위치 2",
-    "genre": "게임",
+    "genre": "슈팅/FPS",
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/GKXS_YA9s7E",
-    "summary": "",
+    "summary": "독특한 능력의 영웅들이 팀을 이뤄 격돌! 탱커·딜러·서포터 역할을 나눠 다양한 모드에서 승리를 노리는 팀 FPS",
     "features": [
       "다양한 영웅 시스템",
-      "영역 배틀",
+      "팀 기반 FPS 전투",
       "빠른 멀티플레이"
     ],
     "image": "images/game20.svg",
@@ -315,10 +315,10 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/haoiSDKghLE",
-    "summary": "",
+    "summary": "99명이 동시에 달리는 배틀로얄 레이싱! 초고속 머신으로 라이벌을 밀어내고 혼자 결승선을 통과하라",
     "features": [
-      "미래형 스피드 액션",
       "배틀로얄 레이싱",
+      "미래형 스피드 액션",
       "온라인 경쟁 시스템"
     ],
     "image": "images/game21.svg",
@@ -331,11 +331,11 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/mIddsPkdX9U",
-    "summary": "\"사라진 전지메기를 찾고, 지하 세계의 비밀을 파헤쳐라!\" 혼돈의 대도시 '카오폴리스'의 신입 대원 3호가 되어, 의문의 액체로 가득 찬 미지의 세계 '얼터너'를 모험하세요!",
+    "summary": "동키콩이 강력한 주먹으로 지형을 직접 파괴하며 나아간다! 바나나를 찾아 지하 깊숙이 파고드는 통쾌한 3D 파괴 액션",
     "features": [
+      "지형 파괴 액션",
       "다채로운 스테이지",
-      "파괴형 액션",
-      "캐주얼 플랫폼 플레이"
+      "파워풀 플랫폼 플레이"
     ],
     "image": "images/game22.svg",
     "en": "Donkey Kong Bananza"
@@ -347,7 +347,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/watch?v=vuSV9Sy28Mw",
-    "summary": "\"사라진 전지메기를 찾고, 지하 세계의 비밀을 파헤쳐라!\" 혼돈의 대도시 '카오폴리스'의 신입 대원 3호가 되어, 의문의 액체로 가득 찬 미지의 세계 '얼터너'를 모험하세요!",
+    "summary": "봉인 전쟁의 과거로 날아간 젤다가 동료를 모아 간논도르프에 맞선다! 젤다 세계관 속 대규모 전장을 압도하는 무쌍 액션",
     "features": [
       "젤다 세계관 스토리",
       "대규모 전장 액션",
@@ -363,10 +363,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/watch?v=j6eNaiftiWE",
-    "summary": "",
+    "summary": "선택 하나가 운명을 바꾼다! 동료와 유대를 쌓으며 치밀한 전략으로 전쟁을 헤쳐나가는 깊이 있는 턴제 전략 RPG",
     "features": [
-      "캐릭터 육성",
       "전략 턴제 전투",
+      "캐릭터 육성",
       "선택형 스토리"
     ],
     "image": "images/game24.svg",
@@ -379,7 +379,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/watch?v=GwOUuC8Y5N0",
-    "summary": "",
+    "summary": "다양한 피크민 군단을 이끌고 조난자를 구출하라! 낯선 행성에서 피크민과 개를 활용해 수수께끼를 풀어가는 전략 탐험 게임",
     "features": [
       "다양한 피크민 활용",
       "전략 탐험 플레이",
@@ -395,10 +395,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/watch?v=zw47_q9wbBE",
-    "summary": "",
+    "summary": "100년의 잠에서 깨어난 링크에게 광활한 하이랄이 기다린다! 어디든 오르고 어떤 방식으로든 싸우는 전례 없는 자유의 오픈 월드",
     "features": [
-      "창의적인 액션 전투",
       "자유로운 오픈월드",
+      "창의적인 액션 전투",
       "광활한 세계 탐험"
     ],
     "image": "images/game26.svg",
@@ -411,11 +411,11 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/watch?v=ZE60HMgVACU",
-    "summary": "",
+    "summary": "자유로운 변신 능력으로 개성 넘치는 세계를 탐험하라! 신비로운 생명체 포코와 함께 떠나는 유쾌하고 발랄한 어드벤처",
     "features": [
-      "어드벤쳐",
+      "자유로운 변신 액션",
       "개성 있는 탐험 플레이",
-      "자유로운 변신 액션"
+      "유쾌한 포켓몬 세계관"
     ],
     "image": "images/game27.svg",
     "en": "Pokopia"
@@ -427,11 +427,11 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/pOfW-qdsvpU",
-    "summary": "",
+    "summary": "세계 최강의 포켓몬 트레이너를 가려라! 엄선한 포켓몬 팀으로 전 세계 플레이어와 실력을 겨루는 정통 전략 배틀",
     "features": [
-      "3인이상",
       "온라인 랭크 매치",
-      "전략 중심 포켓몬 배틀"
+      "전략 중심 포켓몬 배틀",
+      "팀 편성의 재미"
     ],
     "image": "images/game28.svg",
     "en": "Pokemon Champions"
@@ -443,7 +443,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/eNML51GuBew",
-    "summary": "",
+    "summary": "미래의 루미오스 시티가 무대인 새로운 레전드! 활기찬 도시를 누비며 포켓몬과 박진감 넘치는 액션 배틀을 펼치는 오픈 어드벤처",
     "features": [
       "액션 스타일 배틀",
       "도시형 탐험 시스템",
@@ -459,7 +459,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/SPVRNBKaWQY",
-    "summary": "",
+    "summary": "스플래툰 세계관에서 펼쳐지는 새로운 솔로 어드벤처! 잉크 슈팅으로 적을 물리치며 미지의 지역을 탐험하라",
     "features": [
       "스플래툰 스타일 전투",
       "탐험형 슈팅 액션",
@@ -475,7 +475,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/z3puFLFDfxE",
-    "summary": "",
+    "summary": "하늘을 가르는 스피드 레이싱! 커비와 친구들이 개성 넘치는 에어라이드 머신을 타고 맞붙는 신나는 레이싱 배틀",
     "features": [
       "개성 있는 머신 시스템",
       "스피드 레이싱 액션",
@@ -491,7 +491,7 @@ window.GAMES = [
     "players": "2인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/H3LAkr0ANgw",
-    "summary": "",
+    "summary": "인류의 흔적이 남은 미지의 세상에 날아온 커비! 자동차와 자판기를 통째로 삼키는 마우스풀 모드로 적을 물리치는 3D 어드벤처",
     "features": [
       "카피 능력 활용",
       "3D 플랫폼 액션",
@@ -507,10 +507,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/n9FlGHj9-GA",
-    "summary": "",
+    "summary": "최고 속도를 향해 달려라! 30명의 개성 강한 레이서가 극한의 트랙에서 맞붙는 전설의 하드코어 미래형 레이싱",
     "features": [
-      "개성 있는 머신 시스템",
       "초고속 레이싱",
+      "개성 있는 머신 시스템",
       "고난도 트랙 구성"
     ],
     "image": "images/game33.svg",
@@ -523,7 +523,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "",
     "youtube": "https://www.youtube.com/embed/20Hb_onhtUU",
-    "summary": "",
+    "summary": "지구를 탈출한 인류가 외계 행성 '미라'에 불시착! 거대 메카 '스켈'을 타고 드넓은 SF 세계를 탐험하는 오픈 월드 RPG",
     "features": [
       "탑승형 메카 액션",
       "SF 오픈월드 탐험",
@@ -539,7 +539,7 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/GKXS_YA9s7E",
-    "summary": "",
+    "summary": "독특한 능력의 영웅들이 팀을 이뤄 격돌! 탱커·딜러·서포터 역할을 나눠 다양한 모드에서 승리를 노리는 팀 FPS",
     "features": [
       "다양한 영웅 시스템",
       "팀 기반 FPS 전투",
@@ -555,10 +555,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXFJ",
     "youtube": "https://www.youtube.com/embed/02Pf_OHXDHg",
-    "summary": "",
+    "summary": "몬스터를 적이 아닌 동료 '몬스티'로 삼아라! 알을 품어 몬스티를 키우고 함께 가위바위보식 전략 배틀을 펼치는 RPG",
     "features": [
-      "전략형 배틀 플레이",
       "몬스터 동료 시스템",
+      "전략형 배틀 플레이",
       "감성적인 스토리"
     ],
     "image": "images/game36.svg",
@@ -571,10 +571,10 @@ window.GAMES = [
     "players": "3인 이상",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/Hku89o38-hQ",
-    "summary": "",
+    "summary": "두 바퀴를 각각 조작해 펼치는 독특한 팀 스포츠! 휠체어를 타고 골대를 향해 돌진하는 짜릿한 팀 배틀 액션",
     "features": [
-      "팀 기반 스포츠 액션",
       "독특한 조작 시스템",
+      "팀 기반 스포츠 액션",
       "멀티플레이 지원"
     ],
     "image": "images/game37.svg",
@@ -587,9 +587,9 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/spXZHz4ZXUY",
-    "summary": "",
+    "summary": "도마뱀 유카와 박쥐 레일리의 유쾌한 3D 콤비 어드벤처! 광활한 스테이지를 탐험하며 수집 요소를 즐기는 복고풍 플랫폼 게임",
     "features": [
-      "콤비 액션 시스템션",
+      "콤비 액션 시스템",
       "3D 플랫폼 액션",
       "탐험 중심 스테이지"
     ],
@@ -603,10 +603,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXFP",
     "youtube": "https://www.youtube.com/embed/GgXHaZiym9U",
-    "summary": "\"조준(에임)이 너무 어려워!\" → 바닥만 칠해도 팀을 승리로 이끄는 버스 기사 가능! 트렌디한 비주얼과 신나는 락 사운드를 좋아하는 힙스터 바쁜 일상 속, 딱 3분씩만 매콤하게 몰입해서 한 판 즐기고 싶으신 분",
+    "summary": "낮엔 신비로운 블루홀을 잠수하고, 밤엔 스시 레스토랑을 경영하라! 두 가지 라이프를 오가는 독특한 픽셀 어드벤처",
     "features": [
-      "가게 운영 시스템",
       "바다 탐험 플레이",
+      "가게 운영 시스템",
       "독특한 픽셀 감성"
     ],
     "image": "images/game39.svg",
@@ -619,10 +619,10 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXFJ",
     "youtube": "https://www.youtube.com/embed/iofYDsA2yqg",
-    "summary": "",
+    "summary": "셀레스트 산을 정복하려는 매들린의 도전! 불안과 내면의 목소리를 이겨내며 극한의 정밀 플랫폼을 돌파하는 감동 게임",
     "features": [
-      "감성적인 스토리",
       "고난도 플랫폼 액션",
+      "감성적인 스토리",
       "빠른 리트라이 시스템"
     ],
     "image": "images/game40.svg",
@@ -635,7 +635,7 @@ window.GAMES = [
     "players": "1인용",
     "mbti": "XXTP",
     "youtube": "https://www.youtube.com/embed/gX4cGcwmdsY",
-    "summary": "",
+    "summary": "죽어도 다시, 더 강해져서 탈출하라! 머리 없는 죄수가 매번 달라지는 던전을 쾌속으로 돌파하는 로그라이크 액션",
     "features": [
       "속도감 있는 액션",
       "로그라이크 탐험",
